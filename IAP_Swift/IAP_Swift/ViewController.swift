9c9d762985ce9540e263c61cbf7bd9b624193e87
何玉龙 <bryantsisu@gmail.com>
2019-11-03 17:23:03 +0800

    create Xcode initial project

---
+++ IAP_Swift/IAP_Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IAP_Swift
+//
+//  Created by 何玉龙 on 2019/11/3.
+//  Copyright © 2019 com.BryantStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IAP_Swift/IAP_SwiftTests/IAP_SwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  IAP_SwiftTests.swift
+//  IAP_SwiftTests
+//
+//  Created by 何玉龙 on 2019/11/3.
+//  Copyright © 2019 com.BryantStudio. All rights reserved.
+//
+
+import XCTest
+@testable import IAP_Swift
+
+class IAP_SwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
