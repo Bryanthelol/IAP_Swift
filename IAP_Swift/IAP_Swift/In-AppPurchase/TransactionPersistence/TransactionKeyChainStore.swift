737631a8147c549517928c3f2447f774c5a5c9fd
何玉龙 <bryantsisu@gmail.com>
2019-11-03 18:36:55 +0800

    create TransactionPersistence file

---
+++ IAP_Swift/IAP_Swift/In-AppPurchase/TransactionPersistence/TransactionKeyChainStore.swift
@@ -0,0 +1,13 @@
+//
+//  TransactionKeyChainStore.swift
+//  IAP_Swift
+//
+//  Created by 何玉龙 on 2019/11/3.
+//  Copyright © 2019 com.BryantStudio. All rights reserved.
+//
+
+import UIKit
+
+class TransactionKeyChainStore: NSObject {
+
+}
